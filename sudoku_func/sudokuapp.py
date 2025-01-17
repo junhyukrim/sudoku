94b7b35cc5dbf4417758dde6471cceb127e55a29
junhyukrim <junhyukrim@gmail.com>
2025-01-17 13:30:08 +0900

    db_password recommit

--- sudoku_func/sudokuapp.py
+++ sudoku_func/sudokuapp.py
@@ -95,6 +95,7 @@ if call_by_str:
 if call_by_id:
     # with open('password.pw', 'r') as pw_file:
     #     db_password = pw_file.read().strip()
+    #왜 주석 처리가 안되는가? 
 
     database_name = 'sudoku_db'
 
